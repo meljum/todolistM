5e05a98c11d119690169909f7375011bdd43de4c
Amanbek Asylbekov <amanbek.asylbekov@tvh.com>
2022-09-13 19:21:16 +0600

    feat: layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/header/Header"
+import CreateTodo from "./components/create-todo/CreateTodo"
+import TodoItem from "./components/todo-item/TodoItem"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div className='content'>
+        <CreateTodo />
+        <div className='todos-wrapper'>
+            <TodoItem />
+            <TodoItem />
+            <TodoItem />
+            <TodoItem />
+        </div>
+      </div>
     </div>
   );
 }
